7a146e9e0abd20ad01d9e2bc5ee47fd024bd8116
Loeka1234 <loekalievens@hotmail.com>
2020-07-11 21:42:31 +0200

    Server for react

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/dashboard", express.static("web"));
+app.get("/dashboard/*", (_, res) => res.redirect("/dashboard"))
+app.get("/", (_, res) => res.redirect("/dashboard"))
+
+
 app.listen(3000, () => console.log("Listening on localhost:3000"));
