aeee4dc3bda760af2b2becd43e2b7f777fa06030
Loeka1234 <loekalievens@hotmail.com>
2020-07-20 22:45:21 +0200

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -14,21 +14,32 @@ export const register = async (req: Request, res: Response) => {
                 .json({ error: "Email has already been taken. " });
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({
             email,
             name,
             password: hashedPassword,
         });
         await user.save();
+    const hashedPassword = await bcrypt.hash(password.trim(), 10);
+    const user = new User({
+        email,
+        name,
+        password: hashedPassword,
+    });
+    user.save(err => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
         res.status(201).json({ message: "Successfully registred." });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Internal server error." });
-    }
+    });
 };
 
 export const login = async (req: Request, res: Response) => {
     const { email, password }: LoginBody = req.body;
+
+    if(!email && !email.trim()) return res.status(400).json({ error: "Please provide an email." });
+    if(!password && !password.trim()) return res.status(400).json({ error: "Please provide a password." });
+
     let name = "";
     try {
         const user = await User.findOne({ email }).exec();
@@ -65,7 +76,10 @@ export const getNewToken = (req: Request, res: Response) => {
                 process.env.REFRESH_TOKEN_SECRET!,
                 (err: any, user: any) => {
                     if (err) return res.sendStatus(403);
-                    const accessToken = generateAccessToken({ email: user.email, name: user.name });
+                    const accessToken = generateAccessToken({
+                        email: user.email,
+                        name: user.name,
+                    });
                     res.status(200).json({ accessToken });
                 }
             );
@@ -73,12 +87,17 @@ export const getNewToken = (req: Request, res: Response) => {
 };
 
 export const logout = (req: Request, res: Response) => {
-    RefreshToken.findOneAndDelete({ refreshToken: req.body.token }).exec((err, response) => {
-        if(err) res.status(500).json({ error: "Internal server error." });
-        else if(!response) res.status(400).json({ error: "Please provide a refresh token." });
-        else res.status(200).json({ message: "Successfully logged out." });
-    });
-}
+    RefreshToken.findOneAndDelete({ refreshToken: req.body.token }).exec(
+        (err, response) => {
+            if (err) res.status(500).json({ error: "Internal server error." });
+            else if (!response)
+                res.status(400).json({
+                    error: "Please provide a refresh token.",
+                });
+            else res.status(200).json({ message: "Successfully logged out." });
+        }
+    );
+};
 
 function generateAccessToken(user: Ijwt) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET!, {
