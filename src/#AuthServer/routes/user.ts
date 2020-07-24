8b32274b19dbc7f4258e2fdd4a4b4c862534fd33
Loeka1234 <loekalievens@hotmail.com>
2020-07-24 03:23:56 +0200

    Added clearcookie to logout

--- src/#AuthServer/routes/user.ts
+++ src/#AuthServer/routes/user.ts
@@ -110,14 +110,17 @@ export const getNewToken = (req: Request, res: Response) => {
 };
 
 export const logout = (req: Request, res: Response) => {
-    RefreshToken.findOneAndDelete({ refreshToken: req.body.token }).exec(
+    RefreshToken.findOneAndDelete({ refreshToken: req.cookies.jid }).exec(
         (err, response) => {
             if (err) res.status(500).json({ error: "Internal server error." });
             else if (!response)
                 res.status(400).json({
                     error: "Please provide a refresh token.",
                 });
-            else res.status(200).json({ message: "Successfully logged out." });
+            else {
+                res.clearCookie("jid", { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
+                res.status(200).json({ message: "Successfully logged out." });
+            }
         }
     );
 };
